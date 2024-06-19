1286514ff57a12b9517f0ad1a762977ff804f843
nakatake <tonarinototorototoro8972@gmail.com>
2024-06-20 08:51:58 +0900

    add mui

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,35 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
+import { CacheProvider } from "@emotion/react";
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
+import createCache from '@emotion/cache'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { CssBaseline } from "@mui/material";
+import { blue } from "@mui/material/colors";
+
+function createEmotionCache() {
+  return createCache({ key: 'css' })
+}
+
+const cache = createEmotionCache();
+const theme = createTheme({
+  palette: { primary: blue }
+});
+
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <RemixBrowser />
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <RemixBrowser />
+        </ThemeProvider>
+      </CacheProvider>
     </StrictMode>
   );
 });
